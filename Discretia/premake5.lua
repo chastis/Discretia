ece3896c09a6c3168c7aca36e8c93b7f26d64335
Serhii Herashchenko <sergsy.gerashenko@gmail.com>
2020-04-27 18:22:48 +0300

    Restucturised solution directory

---
+++ Discretia/premake5.lua
@@ -0,0 +1,16 @@
+project "Discretia"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.hpp", "**.cpp" }
+
+   cppdialect "C++17"
+   
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
