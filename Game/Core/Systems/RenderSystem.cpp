1be9740f928a7a7cedce0db7863b9860c2a831de
Serhii Herashchenko <sergsy.gerashenko@gmail.com>
2020-05-17 22:26:05 +0300

    Removed systems

--- Game/Core/Systems/RenderSystem.cpp
+++
@@ -1,2 +0,0 @@
-#include <Core/Systems/RenderSystem.h>
-
--- Game/Core/Systems/RenderSystem.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-
-#include <Core/Systems/BaseSystem.h>
-#include <Core/CoreDefs.h>
-
-class RenderSystem : public BaseSystem 
-{
-
-};
